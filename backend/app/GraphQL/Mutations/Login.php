f0c56fd84cac6b2b96bffc3e6ec791f81cb40926
LAPTOP-LK4VNMJ2\HP Laptop <aminnoura@gmail.com>
2021-02-09 19:18:01 +0100

    move items to backend folder

---
+++ backend/app/GraphQL/Mutations/Login.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use Illuminate\Support\Facades\Auth;
+use GraphQL\Error\Error;
+
+class Login
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        
+        $guard = Auth::guard();
+
+        if( ! $guard->attempt($args)) {
+            throw new Error('Invalid credentials.');
+        }
+
+        /**
+         * Since we successfully logged in, this can no longer be `null`.
+         *
+         * @var \App\Models\User $user
+         */
+        $user = $guard->user();
+
+        return $user;
+    }
+}
+ 
