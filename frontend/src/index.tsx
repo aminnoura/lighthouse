160167e8bf20164b6a8545e1a141652101931d12
LAPTOP-LK4VNMJ2\HP Laptop <aminnoura@gmail.com>
2021-02-09 19:44:22 +0100

    add graphql to frontend

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+    ApolloClient,
+    createHttpLink,
+    InMemoryCache,
+    ApolloProvider
+  } from "@apollo/client";
 
 import App from "./App";
 
+export const link = createHttpLink({
+    uri: "http://localhost:8000/graphql"
+});
+
+export const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link,
+});
+
 ReactDOM.render(
-    <App/>,
+    <ApolloProvider client={client}>
+        <App/>
+    </ApolloProvider>,
     document.getElementById("root") as HTMLElement,
 );
