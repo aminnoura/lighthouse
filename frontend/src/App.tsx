c2dd20f1d40fdb414119ccc09d419b3cb9af0cf9
LAPTOP-LK4VNMJ2\HP Laptop <aminnoura@gmail.com>
2021-02-10 20:51:11 +0100

    add header and adding styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import HeaderComponent from "./components/header/Header";
 import Routes from "./utils/router";
+import './App.scss';
 
 const App = () => (
+    <div className="main" >
+        <HeaderComponent />
         <BrowserRouter>
-            <div className="App">
-                <main className="App_content">
-                    <Routes/>
-                </main>
+            <div className="appContainer">
+                <Routes/>
             </div>
         </BrowserRouter>
-    );
+    </div>
+);
 
 export default App;
 
---
+++ frontend/src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './HeaderComponent.scss';
+
+const HeaderComponent = () => (
+    <div className="headerContainer" > 
+        <h1>2025 CHEMNITZ</h1>
+        <button className="languageBtn">Language Change</button>
+    </div>
+);
+
+export default HeaderComponent;
+
+
