12c8ae8c3acff737c002af56a53b5647bd8b364a
LAPTOP-LK4VNMJ2\HP Laptop <aminnoura@gmail.com>
2021-02-11 12:46:09 +0100

    add some styles and use btn component

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import Button from "./button/Button";
 import './HeaderComponent.scss';
 
 const HeaderComponent = () => (
-    <div className="headerContainer" > 
-        <h1>2025 CHEMNITZ</h1>
-        <button className="languageBtn">Language Change</button>
+    <div className="generalContainer1 headerContainer1" >
+        <div className="generalContainer2 headerContainer2">
+            <h1>2025 CHEMNITZ</h1>
+            <Button className="languageBtn" onClick={()=>{console.log('clicked')}} text="Language Button" />
+        </div>
     </div>
 );
 
 export default HeaderComponent;
-
-
